a7149c76b87471c974c4d5912264024339929f48
rwothmio <brwothmio@gmail.com>
2023-08-19 22:37:03 +0300

    change parameters

--- src/components/contact form/handleFormSubmit.js
+++ src/components/contact form/handleFormSubmit.js
@@ -2,31 +2,24 @@ import emailjs from "emailjs-com";
 import isEmail from "validator/lib/isEmail";
 
 const handleFormSubmit = (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    if (!isEmail(e.target.email.value)) {
-      alert("please enter your valid email");
-      return null;
-    }
+  if (!isEmail(e.target.email.value)) {
+    alert("please enter your valid email");
+    return null;
+  }
 
-    emailjs
-      .sendForm(
-        process.env.REACT_APP_EMAIL_JS_SERVICE_ID,
-        process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID,
-        e.target,
-        process.env.REACT_APP_EMAIL_JS_USER_ID
-      )
-      .then((response) => {
-        e.target.reset();
-        console.log("email sent", response);
-        alert("your email has been received");
-      })
-      .catch((err) => {
-        console.warn("email send failed", err);
-        alert(
-          "there was a problem receiving your email. please try that again"
-        );
-      });
-  };
+  emailjs
+    .sendForm("meyo100", "template_a9948nq", e.target, "GcUBuoIEncrpDAYps")
+    .then((response) => {
+      e.target.reset();
+      console.log("email sent", response);
+      alert("your email has been received");
+    })
+    .catch((err) => {
+      console.warn("email send failed", err);
+      alert("there was a problem receiving your email. please try that again");
+    });
+};
 
-  export default handleFormSubmit
+export default handleFormSubmit;
